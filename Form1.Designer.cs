95f9c735c147ff0d9130c826b1c2d84de5ff82a6
Pablo Miranda <pablin330@gmail.com>
2021-08-19 16:44:40 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Simulation_Lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxIterations = new System.Windows.Forms.TextBox();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.labelChi = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAverage = new System.Windows.Forms.TextBox();
+            this.textBoxVariance = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelCurrentIteration = new System.Windows.Forms.Label();
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.labelWeather = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(182, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(343, 91);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(363, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Iterations (N):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(363, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Time (T):";
+            // 
+            // textBoxIterations
+            // 
+            this.textBoxIterations.Location = new System.Drawing.Point(432, 19);
+            this.textBoxIterations.Name = "textBoxIterations";
+            this.textBoxIterations.Size = new System.Drawing.Size(148, 20);
+            this.textBoxIterations.TabIndex = 4;
+            this.textBoxIterations.Text = "4";
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(432, 52);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(148, 20);
+            this.textBoxTime.TabIndex = 5;
+            this.textBoxTime.Text = "60";
+            // 
+            // labelChi
+            // 
+            this.labelChi.Location = new System.Drawing.Point(363, 149);
+            this.labelChi.Name = "labelChi";
+            this.labelChi.Size = new System.Drawing.Size(217, 42);
+            this.labelChi.TabIndex = 6;
+            this.labelChi.Text = "Chi-squared:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(363, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Average:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(363, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Variance:";
+            // 
+            // textBoxAverage
+            // 
+            this.textBoxAverage.Location = new System.Drawing.Point(432, 85);
+            this.textBoxAverage.Name = "textBoxAverage";
+            this.textBoxAverage.ReadOnly = true;
+            this.textBoxAverage.Size = new System.Drawing.Size(148, 20);
+            this.textBoxAverage.TabIndex = 9;
+            // 
+            // textBoxVariance
+            // 
+            this.textBoxVariance.Location = new System.Drawing.Point(432, 118);
+            this.textBoxVariance.Name = "textBoxVariance";
+            this.textBoxVariance.ReadOnly = true;
+            this.textBoxVariance.Size = new System.Drawing.Size(148, 20);
+            this.textBoxVariance.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Simulate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelCurrentIteration
+            // 
+            this.labelCurrentIteration.AutoSize = true;
+            this.labelCurrentIteration.Location = new System.Drawing.Point(9, 179);
+            this.labelCurrentIteration.Name = "labelCurrentIteration";
+            this.labelCurrentIteration.Size = new System.Drawing.Size(85, 13);
+            this.labelCurrentIteration.TabIndex = 12;
+            this.labelCurrentIteration.Text = "Current Iteration:";
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.AutoSize = true;
+            this.labelCurrentTime.Location = new System.Drawing.Point(144, 179);
+            this.labelCurrentTime.Name = "labelCurrentTime";
+            this.labelCurrentTime.Size = new System.Drawing.Size(70, 13);
+            this.labelCurrentTime.TabIndex = 13;
+            this.labelCurrentTime.Text = "Current Time:";
+            // 
+            // labelWeather
+            // 
+            this.labelWeather.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelWeather.Location = new System.Drawing.Point(9, 149);
+            this.labelWeather.Name = "labelWeather";
+            this.labelWeather.Size = new System.Drawing.Size(346, 23);
+            this.labelWeather.TabIndex = 14;
+            this.labelWeather.Text = "Weather for next hour: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 209);
+            this.Controls.Add(this.labelWeather);
+            this.Controls.Add(this.labelCurrentTime);
+            this.Controls.Add(this.labelCurrentIteration);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxVariance);
+            this.Controls.Add(this.textBoxAverage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelChi);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.textBoxIterations);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxIterations;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Label labelChi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxAverage;
+        private System.Windows.Forms.TextBox textBoxVariance;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelCurrentIteration;
+        private System.Windows.Forms.Label labelCurrentTime;
+        private System.Windows.Forms.Label labelWeather;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Simulation_Lab15
+{
+    enum MyWeather
+    {
+        Clear, Cloudy, Overcast 
+    }
+
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DataTable dt = new DataTable();
+            this.dataGridView1.DataSource = dt;
+
+            dt.Columns.Add("Clear");
+            dt.Columns.Add("Cloudy");
+            dt.Columns.Add("Overcast");
+
+            dt.Rows.Add(new Object[] { "-0.4", "0.3", "0.1" });
+            dt.Rows.Add(new Object[] { "0.4", "-0.8", "0.4" });
+            dt.Rows.Add(new Object[] { "0.1", "0.4", "-0.5" });
+
+            this.dataGridView1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int countClear = 0;
+            int countCloudy = 0;
+            int countOvercast = 0;
+
+            int w = 4;
+            double sum = 0;
+
+            int t = int.Parse(textBoxTime.Text);
+            int n = int.Parse(textBoxIterations.Text);
+            
+            // A -- Clear = 0
+            // B -- Cloudy = 1
+            // C -- Overcast = 2
+            double w1 = -0.4; //  Clear->Clear
+            double w2 = -0.8; //  Cloudy->Cloudy
+            double w3 = -0.5;  // Overcast->Overcast
+
+            double w4 = 0.4;  // Cloudy->Clear   -- Overcast->Cloudy -- Cloudy->Overcast
+            double w5 = 0.1;  // Overcast->Clear -- Clear->Overcast
+            double w6 = 0.3;  // Clear->Cloudy
+
+
+            double p1 = 0.33; //in any stage it can start Clear, Cloudy or Overcast
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+
+            int[] weather = new int[w];
+
+            for (int it = 1; it <= n; it++)
+            {
+                labelCurrentIteration.Invoke((MethodInvoker)delegate
+                {
+                    labelCurrentIteration.Text = "Current Iteration: " + it.ToString();
+                    labelCurrentIteration.Update();
+                });
+
+                weather[0] = rand.Next(0, 3);
+                weather[1] = rand.Next(0, 3);
+                weather[2] = rand.Next(0, 3);
+                weather[3] = rand.Next(0, 3);
+
+                labelCurrentTime.Invoke((MethodInvoker)delegate
+                {
+                    labelWeather.Text = "Weather for the next hour: " +
+                        (MyWeather)weather[0] + " -> " +
+                        (MyWeather)weather[1] + " -> " +
+                        (MyWeather)weather[2] + " -> " +
+                        (MyWeather)weather[3];
+                    labelWeather.Update();
+                });
+
+                for (int wi = 0; wi < weather.Length - 1; wi++)
+                {
+                    if (weather[wi] == 0) // Clear
+                    {
+                        countClear++;
+                        if (weather[wi + 1] == 0) // Clear
+                        {
+                            if (wi+1 == 1) p2 = w1;
+                            else if (wi+1 == 2) p3 = w1;
+                            else if (wi + 1 == 3) p4 = w1;
+                        }
+                        else if (weather[wi + 1] == 1) // Cloudy
+                        {
+                            if (wi + 1 == 1) p2 = w6;
+                            else if (wi + 1 == 2) p3 = w6;
+                            else if (wi + 1 == 3) p4 = w6;
+                        }
+                        else if (weather[wi + 1] == 2) // Overcast
+                        {
+                            if (wi + 1 == 1) p2 = w5;
+                            else if (wi + 1 == 2) p3 = w5;
+                            else if (wi + 1 == 3) p4 = w5;
+                        }
+                    }
+                    if (weather[wi] == 1) // Cloudy
+                    {
+                        countCloudy++;
+                        if (weather[wi + 1] == 0 || weather[wi + 1] == 2) // Clear OR Overcast
+                        {
+                            if (wi + 1 == 1) p2 = w4;
+                            else if (wi + 1 == 2) p3 = w4;
+                            else if (wi + 1 == 3) p4 = w4;
+                        }
+                        else if (weather[wi + 1] == 1) // Cloudy
+                        {
+                            if (wi + 1 == 1) p2 = w2;
+                            else if (wi + 1 == 2) p3 = w2;
+                            else if (wi + 1 == 3) p4 = w2;
+                        }
+                    }
+                    if (weather[wi] == 2) // Overcast
+                    {
+                        countOvercast++;
+                        if (weather[wi + 1] == 0) // Clear
+                        {
+                            if (wi + 1 == 1) p2 = w5;
+                            else if (wi + 1 == 2) p3 = w5;
+                            else if (wi + 1 == 3) p4 = w5;
+                        }
+                        else if (weather[wi + 1] == 1) // Cloudy
+                        {
+                            if (wi + 1 == 1) p2 = w4;
+                            else if (wi + 1 == 2) p3 = w4;
+                            else if (wi + 1 == 3) p4 = w4;
+                        }
+                        else if (weather[wi + 1] == 2) // Overcast
+                        {
+                            if (wi + 1 == 1) p2 = w3;
+                            else if (wi + 1 == 2) p3 = w3;
+                            else if (wi + 1 == 3) p4 = w3;
+                        }
+                    }
+                }
+                sum = p1 + p2 + p3 + p4;
+
+                double[] e_probabilities = new double[9];
+                double[] probabilities = new double[9];
+                double[] experiments = new double[9];
+                double[] frequency = new double[9];
+
+                //Expected Value - µ
+                double xp1 = 1 * p1;
+                double xp2 = 2 * p2;
+                double xp3 = 3 * p3;
+                double xp4 = 4 * p4;
+
+                double miu = xp1 + xp2 + xp3 + xp4;
+
+                //Variance - µ
+                double x2p1 = (1 * 1) * p1;
+                double x2p2 = (2 * 2) * p2;
+                double x2p3 = (3 * 3) * p3;
+                double x2p4 = (4 * 4) * p4;
+
+                double variance = x2p1 + x2p2 + x2p3 + x2p4;
+                variance -= (miu * miu);
+
+                experiments[0] = 0;
+                experiments[1] = 0;
+                experiments[2] = 0;
+                experiments[3] = 0;
+
+                e_probabilities[0] = xp1;
+                e_probabilities[1] = xp2;
+                e_probabilities[2] = xp3;
+                e_probabilities[3] = xp4;
+
+                probabilities[0] = p1;
+                probabilities[1] = p2;
+                probabilities[2] = p3;
+                probabilities[3] = p4;
+
+                while (i < n)
+                {
+                    int r = rand.Next(0, 3); // Clear - Cloudy - Overcast
+                    if (r == 1) experiments[0]++;
+                    else if (r == 2) experiments[1]++;
+                    else if (r == 3) experiments[2]++;
+                    else if (r >= 4) experiments[3]++;
+                    i++;
+                }
+
+                i = 0;
+                foreach (var item in experiments)
+                {
+                    frequency[i++] = item / n;
+                }
+
+                double e_miu = 1 * frequency[0] +
+                               2 * frequency[1] +
+                               3 * frequency[2] +
+                               4 * frequency[3];
+
+                double e_variance = (1 * 1) * frequency[0] +
+                                    (2 * 2) * frequency[1] +
+                                    (3 * 3) * frequency[2] +
+                                    (4 * 4) * frequency[3];
+
+                e_variance -= (e_miu * e_miu);
+
+                //Errors
+                double miuError = Math.Abs(e_miu - miu) / Math.Abs(miu);
+                double varianceError = Math.Abs(e_variance - variance) / Math.Abs(variance);
+
+                //Chi
+                double chi = 0;
+                double alfa = 0.05;
+                for (i = 0; i < 4; i++)
+                {
+                    chi += Math.Pow((frequency[i] - probabilities[i]), 2) / probabilities[i];
+                }
+
+                textBoxAverage.Invoke((MethodInvoker)delegate
+                {
+                    textBoxAverage.Text = miu.ToString("0.000") + " (error = " + Math.Round((miuError * 100)).ToString() + "%)";
+                    textBoxAverage.Update();
+                });
+
+
+                textBoxVariance.Invoke((MethodInvoker)delegate
+                {
+                    textBoxVariance.Text = variance.ToString("0.000") + " (error = " + Math.Round((varianceError * 100)).ToString() + "%)";
+                    textBoxVariance.Update();
+                });
+
+
+                labelChi.Invoke((MethodInvoker)delegate
+                {
+                    labelChi.Text = "Chi-squared: \r\n" + chi.ToString("0.000") + " > " + alfa.ToString("0.000") + " ( " + ((chi > alfa) ? "YES" : "FALSE") + " )";
+                    labelChi.Update();
+                });
+
+                for (int ti = 1; ti <= t; ti++)
+                {
+                    labelCurrentTime.Invoke((MethodInvoker)delegate
+                    {
+                        labelCurrentTime.Text = "Current Time: " + ti.ToString();
+                        labelCurrentTime.Update();
+                    });
+                    Thread.Sleep(200);
+                }   
+            }
+        }
+    }
+}
